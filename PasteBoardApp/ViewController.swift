4f7f6e81ec9df0ea3b95806a951354f24723755d
Anderson Malaguitte <anderson.malaguitte@oracle.com>
2020-06-16 15:43:41 +0200

    Initial Commit

---
+++ PasteBoardApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PasteBoardApp
+//
+//  Created by Anderson Malaguitte on 16/06/2020.
+//  Copyright © 2020 Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
