a38ad2858432162740a3765d6c94b5565310cf4b
Anderson <upmalagutti@gmail.com>
2020-06-16 17:08:45 +0200

    caching the contents of the text view (even when the user closes the app)

--- PasteBoardApp/ViewController.swift
+++ PasteBoardApp/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let DATA_KEY = "data_key"
+    
     @IBOutlet weak var textView: UITextView!
     
     var pastedText: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let loadedText = UserDefaults.standard.stringArray(forKey: DATA_KEY) {
+            pastedText.append(contentsOf: loadedText)
+        }
         showText()
     }
     
@@ -24,7 +30,9 @@ class ViewController: UIViewController {
         guard let text = UIPasteboard.general.string, !pastedText.contains(text) else {
             return
         }
+        
         pastedText.append(text)
+        UserDefaults.standard.set(pastedText, forKey: DATA_KEY)
         showText()
     }
     
